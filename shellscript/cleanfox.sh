02dab871c8b28d3ceab298b1f4bab9568f8dca89
send <kaz.july.7@gmail.com>
2009-04-28 18:22:18 +0900

    vacuum and reindex utility for firefox

---
+++ shellscript/cleanfox.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+home=/Users/kazuaki
+app_bin=firefox-bin
+app_name=/Applications/Firefox.app
+sqlite=/opt/local/bin/sqlite3
+pgrep=/opt/local/bin/pgrep
+try_limit=30
+open=/usr/bin/open
+
+tried=0
+while true ; do
+  ps=`$pgrep $app_bin`
+  if [ -z "$ps" ] ; then
+    break
+  fi
+  kill $ps
+  tried=`expr $tried + 1`
+  if [ $tried -gt $try_limit ] ; then
+    echo "exceeded retry limit"
+    echo "tried :$tried"
+    echo "limit :$try_limit"
+    exit 1
+  fi
+  sleep 2
+done
+for dir in $home/Library/Application\ Support/Firefox/Profiles/*  $home/Library/Caches/Firefox/Profiles/* ; do
+  if [ -d "$dir" ] ; then
+    cd "$dir"
+    for f in *.sqlite ; do
+      $sqlite $f vacuum .exit
+      $sqlite $f reindex .exit
+    done
+  fi
+done
+$open -a $app_name

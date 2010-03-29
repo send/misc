a432aaf6530c729ad9777b11c77404621a5eb956
SAKAI, Kazuaki <kaz.july.7@gmail.com>
2010-03-29 22:57:09 +0900

    add createDocument

--- firefox/greasemonkey/expandowlyurl.user.js
+++ firefox/greasemonkey/expandowlyurl.user.js
@@ -16,11 +16,29 @@
         method: 'GET',
         url: anchor.getAttribute('href'),
         onload: function(xhr) {
-          var iframe = xhr.responseXML.getElementById('hootFrame');
+          var dom = createDocument(xhr.responseText);
+          var iframe = dom.getElementById('hootFrame');
           console.log(iframe.getAttribute('src'));
           anchor.setAttribute('href', iframe.getAttribute('src'));
         }
       });
     },0);
   });
+  function createDocument(str) {
+    if (document.documentElement.nodeName != 'HTML') 
+      return new DOMParser().parseFromString(str, 'application/xhtml+xml');
+    var html = str.replace(/^[\s\S]*?<html(?:[ \t\r\n][^>]*)?>|<\/html[ \t\r\n]*>[\w\W]*$/ig, '');
+    var doc = document.implementation.createDocument(null, 'html', null);
+    var range = document.createRange();
+    range.setStartAfter(document.body);
+    var fragment = range.createContextualFragment(html);
+    try {
+      fragment = doc.adoptNode(fragment);
+    } catch (e) {
+      fragment = doc.importNode(fragment, true);
+    }
+    doc.documentElement.appendChild(fragment);
+    return doc;
+  }
+  
 })();
